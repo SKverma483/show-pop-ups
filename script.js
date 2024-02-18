ddf420568f4119afa26b148813a69d68e5398bb5
SHUBHAM KUMAR VERMA <132213831+SKverma483@users.noreply.github.com>
2024-02-18 08:48:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const closeModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 const showModal = document.querySelectorAll(".show-modal");
 for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener("click", showingTheModals);
+  showModal[i].addEventListener("click", togglingTheModals);
 }
 closeModal.addEventListener("click", togglingTheModals);
 overlay.addEventListener("click", togglingTheModals);
